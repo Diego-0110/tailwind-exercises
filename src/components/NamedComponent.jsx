d12afcb06bc1c17befc5fc70a707dc10ec6ad5ab
Diego <diegobqu@ucm.es>
2023-11-28 11:58:21 +0100

    .gitignore applied

--- src/components/NamedComponent.jsx
+++
@@ -1,8 +0,0 @@
-export default function NamedComponent ({ name, children, ...props }) {
-  return (
-    <div className="flex flex-col items-center gap-1" {...props}>
-      {children}
-      <p className="font-bold text-sm text-gray-400">{name}</p>
-    </div>
-  )
-}
